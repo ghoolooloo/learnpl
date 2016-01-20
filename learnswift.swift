8a1742e6abea8dd18362070dad90c29b253fb410
Jo <systemadministrator@126.com>
2016-01-20 15:07:01 +0800

    元组

--- learnswift.swift
+++ learnswift.swift
@@ -114,6 +114,17 @@ var maxAmplitudeFound = AudioSample.min
 // 类型别名与原类型是完全兼容的
 
 
+// 元组
+// 元组（tuples）把多个值组合成一个复合值。元组内的值可以是任意类型，并不要求是相同类型。
+let http404Error = (404, "Not Found")  // http404Error 的类型是 (Int, String)，值是 (404, "Not Found")
+let (justTheStatusCode, _) = http404Error  // 可以通过模式匹配的方式提取元组中的值。提取的时候可以把要忽略的部分用下划线（_）标记
+print("The status code is \(justTheStatusCode)")  // 输出 "The status code is 404"
+// 还可以通过下标来访问元组中的单个元素，下标从零开始：
+print("The status message is \(http404Error.1)")  // 输出 "The status message is Not Found"
+let http200Status = (statusCode: 200, description: "OK")  // 可以在定义元组的时候给单个元素命名
+print("The status code is \(http200Status.statusCode)")   // 输出 "The status code is 200"
+
+
 /*
     Optionals 是 Swift 的新特性，它允许你存储两种状态的值给 Optional 变量：有效值或 None 。
     可在值名称后加个问号 （？） 来表示这个值是 Optional。
